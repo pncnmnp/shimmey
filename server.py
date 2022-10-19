05a0e4dfd250cd8e1e620df8e357969bcddb13c4
Parth Parikh <parthparikh1999p@gmail.com>
2022-10-19 03:59:42 -0400

    Add AJAX POST call For communication with Flask

--- server.py
+++ server.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, request
 
 app = Flask(__name__, 
             template_folder="./flask/templates/", 
             static_folder="./flask/static/")
 
-@app.route('/', methods=["GET"])
+@app.route('/')
 def home():
-    try:
-        return render_template("index.html", k=3, offsets=[0, 10, 15], len=10**5)
-    except:
-        abort(404)
+    return render_template("index.html", k=3, offsets=[0, 10, 15], len=10**5)
+
+@app.route('/path', methods=["POST"])
+def view():
+    data = request.get_json()
+    print(data)
+    return data
