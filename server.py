689f9bcc4f09c7f00c7584d95c3b3f7a4c0b0281
Parth Parikh <parthparikh1999p@gmail.com>
2022-10-19 12:29:57 -0400

    Add PIR on server using malicious urls list Generate random bitset on client

--- server.py
+++ server.py
@@ -1,15 +1,51 @@
-from flask import Flask, render_template, abort, request
+from bitarray import bitarray
+from flask import Flask, render_template, request
+from math import sqrt
+import pandas as pd
+from pybloom import BloomFilter
 
-app = Flask(__name__, 
-            template_folder="./flask/templates/", 
-            static_folder="./flask/static/")
+app = Flask(
+    __name__, template_folder="./flask/templates/", static_folder="./flask/static/"
+)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template("index.html", k=3, offsets=[0, 10, 15], len=10**5)
+    return render_template("index.html", k=3, offsets=[0, 10, 15], len=10**6)
 
-@app.route('/path', methods=["POST"])
-def view():
-    data = request.get_json()
-    print(data)
-    return data
+
+@app.route("/main", methods=["POST"])
+def main_server():
+    data = request.get_json()["data"]
+    random_subset = bitarray(data, endian="little")
+    column = pir(random_subset)
+    return column
+
+
+@app.route("/sec", methods=["POST"])
+def sec_server():
+    data = request.get_json()["data"]
+    random_subset = bitarray(data, endian="little")
+    column = pir(random_subset)
+    return column
+
+
+def pir(random_subset):
+    bf = malicious_urls()
+    skip = int(sqrt(bf.capacity))
+    PIR_column = []
+    for i in range(0, bf.capacity, skip):
+        inter = bf.bitarray[i : i + skip] ^ random_subset
+        bit_val = 0
+        for bit in inter:
+            bit_val = bit ^ bit_val
+        PIR_column.append(str(bit_val))
+    return "".join(PIR_column)
+
+
+def malicious_urls(path="./malicious_urls.csv"):
+    df = pd.read_csv(path)
+    bf = BloomFilter(capacity=10**6, error_rate=0.001)
+    for url in df["Domain"]:
+        bf.add(url)
+    return bf
